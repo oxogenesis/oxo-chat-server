7a881cd949884a9189f917cf849585abd3806c21
obeTvR9XDbUwquA6JPQhmbgaCCaiFa2rvf <namespacegenesis@gmail.com>
2023-09-24 21:32:49 +0800

    Add files via upload

---
+++ web/app/sitemap.js
@@ -0,0 +1,62 @@
+import { Json2Str } from '@/lib/Util'
+
+import prisma from '@/lib/prisma'
+
+export default async function sitemap() {
+  let bulletins_url = `${process.env.BASE_URL}/bulletins`
+  let bulletins = await prisma.BULLETINS.findMany({
+    select: {
+      hash: true,
+      signed_at: true
+    }
+  })
+  bulletins = Json2Str(bulletins)
+  bulletins = JSON.parse(bulletins)
+  bulletins = bulletins.map((bulletin) => (
+    {
+      url: `${bulletins_url}/${bulletin.hash}`,
+      lastModified: new Date(parseInt(bulletin.signed_at)),
+      changeFrequency: 'weekly',
+      priority: 0.5
+    }
+  ))
+
+  let accounts_url = `${process.env.BASE_URL}/accounts`
+  let accounts = await prisma.BULLETINS.groupBy({
+    by: ['address'],
+    _count: {
+      hash: true,
+    },
+    orderBy: {
+      address: 'desc',
+    }
+  })
+  accounts = Json2Str(accounts)
+  accounts = JSON.parse(accounts)
+  accounts = accounts.map((account) => (
+    {
+      url: `${accounts_url}/${account.address}/bulletins`,
+      changeFrequency: 'weekly',
+      priority: 0.6
+    }
+  ))
+
+  let site_urls = [
+    {
+      url: process.env.BASE_URL,
+      changeFrequency: 'daily',
+      priority: 1
+    },
+    {
+      url: accounts_url,
+      changeFrequency: 'daily',
+      priority: 1
+    },
+    {
+      url: bulletins_url,
+      changeFrequency: 'daily',
+      priority: 1
+    }
+  ]
+  return [...site_urls, ...accounts, ...bulletins]
+}
